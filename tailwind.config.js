4ed2e958ab6e2b6f176384b8e4868e7dd9187745
Matt Odille <matt.odille@gmail.com>
2024-11-19 01:44:06 -0500

    Convert the home page and about page

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,20 @@ module.exports = {
     extend: {
       colors: {},
     },
+    // fontSize: {
+    //   sm: '0.8rem',
+    //   base: '1rem',
+    //   xl: '1.25rem',
+    //   '2xl': '1.563rem',
+    //   '3xl': '1.953rem',
+    //   '4xl': '2.441rem',
+    //   '5xl': '3.052rem',
+    // }
   },
+  darkMode: 'media',
   variants: {},
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [
+    require("@tailwindcss/typography"),
+    require('flowbite/plugin')
+  ],
 };
