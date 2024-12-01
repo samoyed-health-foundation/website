dbe7671284432d9411c77aa6bb6f22a9b806f24e
Matt Odille <matt.odille@gmail.com>
2024-12-01 16:46:45 -0500

    Upgrade to latest eleventy and install markdown-it plugins

---
+++ eleventy.config.js
@@ -0,0 +1,56 @@
+import markdownIt from "markdown-it";
+import markdownItAnchor from "markdown-it-anchor";
+import markdownItAttrs from "markdown-it-attrs";
+import markdownItTableOfContents from "markdown-it-table-of-contents";
+import yaml from "js-yaml";
+import { DateTime } from "luxon";
+
+export default async function (eleventyConfig) {
+  // https://www.11ty.dev/docs/ignores/#opt-out-of-using-.gitignore
+  eleventyConfig.setUseGitIgnore(false);
+
+  // Use custom markdown-it instance so it can be set as a filter, too
+  const markdownLib = markdownIt({ html: true })
+    .use(markdownItAttrs)
+    .use(markdownItAnchor)
+    .use(markdownItTableOfContents);
+  eleventyConfig.setLibrary("md", markdownLib);
+  eleventyConfig.addFilter("markdown", (content) => {
+    return markdownLib.render(content);
+  });
+
+  // Human-readable dates
+  eleventyConfig.addFilter("readableDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat(
+      "dd LLL yyyy"
+    );
+  });
+
+  // https://www.11ty.dev/docs/data-custom/#yaml
+  eleventyConfig.addDataExtension("yaml", (contents) => yaml.load(contents));
+
+  // https://www.11ty.dev/docs/copy/
+  // Copy files to /_site
+  eleventyConfig.addPassthroughCopy({
+    "./src/admin/config.yml": "./admin/config.yml",
+    "./node_modules/alpinejs/dist/cdn.min.js": "./static/js/alpine.js",
+  });
+  // Copy static folder to /_site
+  eleventyConfig.addPassthroughCopy({ "src/static/css": "css" });
+  eleventyConfig.addPassthroughCopy({ "src/static/files": "files" });
+  eleventyConfig.addPassthroughCopy({ "src/static/img": "img" });
+  eleventyConfig.addPassthroughCopy({ "src/static/webfonts": "webfonts" });
+  // Copy favicon to /_site
+  eleventyConfig.addPassthroughCopy("./src/favicon.ico");
+
+  return {
+    // https://www.11ty.dev/docs/config/#input-directory
+    dir: {
+      input: "src",
+    },
+    templateFormats: ["html", "md", "njk"],
+    // https://www.11ty.dev/docs/config/#default-template-engine-for-html-files
+    // Use nunjucks instead of liquid for .html files
+    htmlTemplateEngine: "njk",
+  };
+};
