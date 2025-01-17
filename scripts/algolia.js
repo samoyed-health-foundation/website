42feb933c85ab91d34b6eb9b62f4adb5d264a99f
Matt Odille <matt.odille@gmail.com>
2025-01-16 22:18:50 -0500

    Perform dry-run of algolia indexing during CI

--- scripts/algolia.js
+++ scripts/algolia.js
@@ -2,6 +2,8 @@ import fs from 'fs';
 import path from 'path';
 import indexing from 'algolia-indexing';
 
+const isDryRun = process.argv.includes("--dry-run");
+
 try {
   // Read the index file
   // (Assumes the Eleventy site has been built, and thus the index file exists)
@@ -9,6 +11,10 @@ try {
   const fileContents = fs.readFileSync(indexFilePath, 'utf-8');
   const records = JSON.parse(fileContents);
   console.log('Algolia index file loaded successfully:', records.length);
+  if (records.length < 100) {
+    console.error('Unusually low number of records found in the Algolia index file');
+    process.exit(1);
+  }
 
   // Send to Algolia
   // https://github.com/pixelastic/algolia-indexing
@@ -19,8 +25,22 @@ try {
   };
   const settings = { searchableAttributes: ['title', 'summary'] };
   indexing.verbose();
-  await indexing.fullAtomic(credentials, records, settings);
-  console.log('Algolia index updated successfully');
+
+  if (isDryRun) {
+    console.log("Indexing in dry-run mode...");
+    if (typeof indexing.fullAtomic === "function" && indexing.fullAtomic.length === 3) {
+      console.log("Check passed: `indexing.fullAtomic` is a function that takes 3 arguments");
+      process.exit(0);
+    } else {
+      console.error("Check failed: `indexing.fullAtomic` is not a function that takes 3 arguments");
+      process.exit(1);
+    }
+  } else {
+    console.log("Running full indexing operation...");
+    await indexing.fullAtomic(credentials, records, settings);
+    console.log('Algolia index updated successfully');
+  }
 } catch (error) {
   console.error('Failed to update Algolia index:', error);
+  process.exit(1);
 }
